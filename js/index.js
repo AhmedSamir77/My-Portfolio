e805b3a668a5d99c5ff6ca8be8b7a294263cb337
ahmed samir <ahmedSamir@gmail.com>
2024-05-29 16:45:31 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,59 @@
+// change color of navbar && Up Icon
+let { top: aboutTop } = $("#about").offset();
+
+$(window).scroll(function () {
+  let scrollTop = $(window).scrollTop();
+
+  if (scrollTop > aboutTop - 100) {
+    $("#navbar").css({ backgroundColor: "black" });
+    $(".upIcon").fadeIn(1000);
+  } else {
+    $("#navbar").css({ backgroundColor: "transparent" });
+    $(".upIcon").fadeOut(1000);
+  }
+});
+
+$(".upIcon").click(function () {
+  // $(window).scrollTop(0); supported by bootstrap only
+
+  $("html, body").animate({ scrollTop: 0 }, 1000);
+});
+
+$(document).ready(function () {
+  $(".colorBoxIcon").click(function () {
+    let colorBoxWidth = $(".colorBox-content").outerWidth();
+    let colorBoxOffset = $(".colorBox").offset().left;
+
+    if (colorBoxOffset < 0) {
+      $(".colorBox").animate({ left: 0 }, 1000);
+    } else {
+      $(".colorBox").animate({ left: -colorBoxWidth }, 1000);
+    }
+  });
+
+  function randomColor() {
+    let red = Math.floor(Math.random() * 255);
+    let green = Math.floor(Math.random() * 255);
+    let blue = Math.floor(Math.random() * 255);
+
+    return `rgb(${red},${green},${blue})`;
+  }
+
+  let spans = $(".colorBox-content span");
+  spans.each(function () {
+    $(this).css("backgroundColor", randomColor());
+  });
+
+  spans.click(function (e) {
+    let currentColor = $(e.target).css("backgroundColor");
+
+    $("h1, h2").css("color", currentColor);
+    $(":root").css("--main-color", currentColor);
+  });
+
+  $(".loader").fadeOut(600, function () {
+    $(".loading").fadeOut(1000, function () {
+      $("body").css("overflow", "auto");
+    });
+  });
+});
